cea255e6100c67ec321bd07380814ab6e125c15d
frank <thmsfrank04@gmail.com>
2023-05-21 18:30:03 -0700

    Complete assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+ def name=(name)
+    @name = name 
+    puts "Own name now is  #{@name}"
+ end
 
+ def name
+    @name
+    end
+
+ def breed=(breed)
+    @breed = breed
+    puts "Own breed now is  #{@breed}"
+    end
+
+ def breed 
+    @breed
+    end
 end
